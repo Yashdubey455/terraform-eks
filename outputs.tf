c704aa80d25b319ab1bab518d230d12c641002b1
Yash Dubey <126498286+Yashdubey455@users.noreply.github.com>
2026-05-14 14:01:02 +0530

    Add outputs for cluster endpoint and OIDC issuer

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,10 @@ output "cluster_name" {
   value = aws_eks_cluster.eks.name
 }
 
+output "cluster_endpoint" {
+  value = aws_eks_cluster.eks.endpoint
+}
+
+output "cluster_oidc_issuer" {
+  value = aws_eks_cluster.eks.identity[0].oidc[0].issuer
+}
